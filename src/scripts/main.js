65e809bf036f2ab1ba47a3314032ff36b194c9a0
Mykola Butylkov <80322190+MykolaButylkov@users.noreply.github.com>
2023-07-07 14:28:31 +0300

    menu close have been fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const toggler = document.querySelector('.menu-button');
+const navMenuList = document.querySelectorAll('.nav-buttom__link');
 const menu = document.querySelector('.nav-buttom');
 
 toggler.addEventListener('click', () => {
   menu.classList.toggle('nav-buttom--laptop');
+});
+
+navMenuList.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.toggle('nav-buttom--laptop');
+  });
 });
 
 const form = document.querySelector('form');
